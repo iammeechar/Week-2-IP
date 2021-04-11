dc84164eb716627d6c0747ee1959c351eadfae99
iammeechar <aldomicha@gmail.com>
2021-04-11 15:35:04 +0300

    add array containing number of days

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,4 @@
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 varmaleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw","Kofi","Kwame"];
+//create an array denoting the different days of the week
+var day = ["0", "1", "2", "3", "4", "5","6"];
