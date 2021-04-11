192e5b2974e622f5ac9a781a9794ab7e9800109a
iammeechar <aldomicha@gmail.com>
2021-04-11 16:07:05 +0300

    add variables relating to html id/names

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,12 @@ var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 varmaleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw","Kofi","Kwame"];
 //create an array denoting the different days of the week
 var day = ["0", "1", "2", "3", "4", "5","6"];
+//create function to evaluate input from user 
+
+function evaluateDate()
+{
+    var date = new Date(form.birthday.value);
+    var day = date.getDay();
+    var result;
+    var gender = form.gender.value
+}
