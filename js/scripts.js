2d8e0cdaec93e922ebdbfd81887f9fb97afee0d5
iammeechar <aldomicha@gmail.com>
2021-04-11 21:20:16 +0300

    change form name to Akan

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,24 @@
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-varmaleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw","Kofi","Kwame"];
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw","Kofi","Kwame"];
 //create an array denoting the different days of the week
 var day = ["0", "1", "2", "3", "4", "5","6"];
 //create function to evaluate input from user 
 
 function evaluateDate()
 {
-    var date = new Date(form.birthday.value);
+    var date = new Date(Akan.birthday.value);
     var day = date.getDay();
     var result;
-    var gender = form.gender.value
+    var gender = Akan.gender.value
+
+    if ( gender == "male")
+    {
+        result = maleNames[day];//array in variable to denote the day of the month
+    }
+    else if( gender =="female")
+    {
+        result = femaleNames[day];//array in variable to denote the day of the month
+    }
+    document.getElementById("result").innerHTML = result;
+
 }
